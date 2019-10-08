4c21b80eabdd99deebaeb1ab012a1eeb4bded437
fahri <fahrisafari95@gmail.com>
2019-10-08 13:24:17 +0700

    route web

---
+++ app/Http/Controllers/RegistrasiController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class RegistrasiController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+   
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function registrasi_pembeli()
+    {
+		echo "wkwkwkwk";die();
+        $pageTitle = "Beranda";
+        return view('home',compact('pageTitle'));
+    }
+	
+	public function gantipass()
+    {
+		$idx = Auth::user()->id;
+		$queryxp = DB::select("select * from users where id='".$idx."'");
+		$pageTitle = "Ganti Password";
+        return view('gantipass',compact('pageTitle','queryxp'));
+	}
+	
+	public function updatepass(Request $request)
+    {
+		// echo bcrypt($request->password);die();
+		$queryxp = DB::select("
+		update users set password='".bcrypt($request->password)."' , password_real='".$request->password."' 
+		where id='".Auth::user()->id."'
+		");
+		return redirect('');
+		
+	}
+}
