8ca464cdabb7cf081b8d27a776c1592090bc52cd
fahri <fahrisafari95@gmail.com>
2019-10-21 11:22:51 +0700

    yajra management eksportir importir perwakilan

--- app/Http/Controllers/VerifyuserController.php
+++ app/Http/Controllers/VerifyuserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Auth;
 use App\Http\Controllers\Controller;
+use Yajra\DataTables\Facades\DataTables;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
@@ -24,6 +25,143 @@ class VerifyuserController extends Controller
         $pageTitle = "Importir";
 		$data = DB::select("select a.*,a.id as ida,a.status as status_a,b.* from itdp_company_users a, itdp_profil_imp b where a.id_profil = b.id and id_role='3' order by a.id desc ");
         return view('verifyuser.index2', compact('pageTitle','data'));
+    }
+	
+	 public function getimportir()
+    {
+      $pesan = DB::select("select ROW_NUMBER() OVER (ORDER BY a.id DESC) AS Row, a.*,a.id as ida,a.status as status_a,b.* from itdp_company_users a, itdp_profil_imp b where a.id_profil = b.id and id_role='3' order by a.id desc ");
+      return DataTables::of($pesan)
+            ->addColumn('f1', function ($pesan) {
+				 return $pesan->company;
+            })
+			->addColumn('f2', function ($pesan) {
+				 return $pesan->addres;
+            })
+			->addColumn('f3', function ($pesan) {
+				 return $pesan->postcode;
+            })
+			->addColumn('f4', function ($pesan) {
+				 return $pesan->phone;
+            })
+			->addColumn('f5', function ($pesan) {
+				 return $pesan->fax;
+            })
+			->addColumn('f6', function ($pesan) {
+				 if($pesan->agree == 1){ 
+				 return "<center><font color='green'>Sudah</font></center>";
+				 }else{ 
+				 return "<center><font color='red'>Belum</font></center>";
+				 }
+            })
+			->addColumn('f7', function ($pesan) {
+				if($pesan->status_a == 1){ 
+				return "<center><font color='green'>Verified</font></center>";
+				} else if($pesan->status_a == 2){ 
+				return "<center><font color='red'>Not Verified</font></center>";
+				}else{ 
+				return "<center><font color='orange'>Wait Administrator</font></center>";
+				}
+				 
+            })
+            ->addColumn('action', function ($pesan) {
+           
+                if($pesan->status_a == 1 || $pesan->status_a == 2){ 
+				return '<a href="'.url('profil2/'.$pesan->id_role.'/'.$pesan->ida).'" class="btn btn-sm btn-info"><i class="fa fa-edit text-white"></i> Detail</a>';
+				}else{
+				return '<a href="'.url('profil2/'.$pesan->id_role.'/'.$pesan->ida).'" class="btn btn-sm btn-success"><i class="fa fa-edit text-white"></i> Verify</a>';
+				}
+               
+                
+           
+                
+            })
+			->rawColumns(['action','f6','f7'])
+            ->make(true);
+    }
+	
+	public function geteksportir()
+    {
+      $pesan = DB::select("select ROW_NUMBER() OVER (ORDER BY a.id DESC) AS Row, a.*,a.id as ida,a.status as status_a,b.* from itdp_company_users a, itdp_profil_imp b where a.id_profil = b.id and id_role='2' order by a.id desc ");
+      return DataTables::of($pesan)
+            ->addColumn('f1', function ($pesan) {
+				 return $pesan->company;
+            })
+			->addColumn('f2', function ($pesan) {
+				 return $pesan->addres;
+            })
+			->addColumn('f3', function ($pesan) {
+				 return $pesan->postcode;
+            })
+			->addColumn('f4', function ($pesan) {
+				 return $pesan->phone;
+            })
+			->addColumn('f5', function ($pesan) {
+				 return $pesan->fax;
+            })
+			->addColumn('f6', function ($pesan) {
+				 if($pesan->agree == 1){ 
+				 return "<center><font color='green'>Sudah</font></center>";
+				 }else{ 
+				 return "<center><font color='red'>Belum</font></center>";
+				 }
+            })
+			->addColumn('f7', function ($pesan) {
+				if($pesan->status_a == 1){ 
+				return "<center><font color='green'>Verified</font></center>";
+				} else if($pesan->status_a == 2){ 
+				return "<center><font color='red'>Not Verified</font></center>";
+				}else{ 
+				return "<center><font color='orange'>Wait Administrator</font></center>";
+				}
+				 
+            })
+            ->addColumn('action', function ($pesan) {
+           
+                if($pesan->status_a == 1 || $pesan->status_a == 2){ 
+				return '<a href="'.url('profil2/'.$pesan->id_role.'/'.$pesan->ida).'" class="btn btn-sm btn-info"><i class="fa fa-edit text-white"></i> Detail</a>';
+				}else{
+				return '<a href="'.url('profil2/'.$pesan->id_role.'/'.$pesan->ida).'" class="btn btn-sm btn-success"><i class="fa fa-edit text-white"></i> Verify</a>';
+				}
+               
+                
+           
+                
+            })
+			->rawColumns(['action','f6','f7'])
+            ->make(true);
+    }
+	
+	public function getpw()
+    {
+      $pesan = DB::select("select ROW_NUMBER() OVER (ORDER BY id DESC) AS Row, * from itdp_admin_users where id_group='4' order by id desc");
+      return DataTables::of($pesan)
+            ->addColumn('f1', function ($pesan) {
+				 return $pesan->name;
+            })
+			->addColumn('f2', function ($pesan) {
+				 return $pesan->email;
+            })
+			->addColumn('f3', function ($pesan) {
+				 return $pesan->website;
+            })
+			->addColumn('f6', function ($pesan) {
+				 if($pesan->id_admin_ln == null || $pesan->id_admin_ln == 0){ 
+				 return "<center>Dalam Negeri</center>";
+				 }else{ 
+				 return "<center>Luar Negeri</center>";
+				 }
+            })
+			->addColumn('f7', function ($pesan) {
+				return "<center>".$pesan->type."</center>";
+				 
+            })
+            ->addColumn('action', function ($pesan) {
+           
+               return '<center><a class="btn btn-danger" href="'.url('hapusperwakilan/'.$pesan->id).'"><i class="fa fa-trash"></i> Hapus</a></center>';
+				
+            })
+			->rawColumns(['action','f6','f7'])
+            ->make(true);
     }
 	
 	public function tambahperwakilan()
@@ -197,7 +335,15 @@ class VerifyuserController extends Controller
 		, id_mst_province='".$request->province."' , postcode='".$request->postcode."', fax='".$request->fax."', website='".$request->website."', phone='".$request->phone."' , status='".$request->staim."'
 		where id='".$id_user_b."'");
 		
+		if($request->staim == 2){
+			if($request->template_reject == 1){
+				$updatetabz = DB::select("update itdp_company_users set id_template_reject='".$request->template_reject."', keterangan_reject='".$request->txtreject."'  where id='".$request->id_user."' ");
 		
+			}else{
+				$updatetabz = DB::select("update itdp_company_users set id_template_reject='".$request->template_reject."'  where id='".$request->id_user."' ");
+		
+			}
+		}
 		
 		return redirect('profil2/'.$id_role.'/'.$id_user);
 		
