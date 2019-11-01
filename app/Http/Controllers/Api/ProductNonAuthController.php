d60b073c21f8387cb3832776dc83dbec24092a75
Yudha <yudha.afuwwu.m@gmail.com>
2019-11-01 10:41:21 +0700

    tes image api

--- app/Http/Controllers/Api/ProductNonAuthController.php
+++ app/Http/Controllers/Api/ProductNonAuthController.php
@@ -98,8 +98,8 @@ class ProductNonAuthController extends Controller
 
 	 public function getImageProduk(Request $request){
 		$path = public_path().'/uploads/Eksportir_Product/Image/'.$request->id.'/'.$request->image;
-		dd($path);
+		//dd($path);
 		
-        return response($path);        
+        return Response::download($path);        
     }
 }
