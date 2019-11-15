eb19d3102e9ef01bb5fa3a4e617b79ca91e148d6
fahri <fahrisafari95@gmail.com>
2019-11-15 10:44:30 +0700

    error saat pilih eksportir di buy request

---
+++ resources/views/UM/user/emailbr.blade.php
@@ -0,0 +1,9 @@
+<p>Dear user, </P>
+<p>{{$nama}} was created Buy Request</p>
+<!-- <ol>
+    <ul>Name : {{$nama}}</ul>
+    <ul>Username : {{$username}}</ul>
+    <ul>Password : {{$password}}</ul>
+</ol> -->
+If you want join <br>
+You can check on <a href="{{url('br_list/')}}">here</a> !.
