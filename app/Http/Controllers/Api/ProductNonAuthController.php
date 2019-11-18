0144f65247f79f888c7a275a853303d20868170e
ilyas <ilyasmuhammad97@gmail.com>
2019-11-18 19:36:03 +0700

    ilyas

--- app/Http/Controllers/Api/ProductNonAuthController.php
+++ app/Http/Controllers/Api/ProductNonAuthController.php
@@ -17,20 +17,21 @@ class ProductNonAuthController extends Controller
     // public function __construct()
     // {
     //    auth()->shouldUse('api_user');
-	// }
+    // }
 
 
-	public function browseProduct(){
-		$dataProduk = DB::table('itdp_company_users')
-							->join('csc_product_single', 'itdp_company_users.id', '=', 'csc_product_single.id_itdp_company_user')
-							->where('itdp_company_users.status', '=', 1)
-							->where('csc_product_single.status', 2)
-							->select('csc_product_single.id', 'csc_product_single.prodname_en',
-							'csc_product_single.image_1', 'csc_product_single.id_csc_product', 'itdp_company_users.type')
-							->inRandomOrder()
-            				->limit(10)
-							->get();
-		if(count($dataProduk) > 0){
+    public function browseProduct()
+    {
+        $dataProduk = DB::table('itdp_company_users')
+            ->join('csc_product_single', 'itdp_company_users.id', '=', 'csc_product_single.id_itdp_company_user')
+            ->where('itdp_company_users.status', '=', 1)
+            ->where('csc_product_single.status', 2)
+            ->select('csc_product_single.id', 'csc_product_single.prodname_en',
+                'csc_product_single.image_1', 'csc_product_single.id_csc_product', 'itdp_company_users.type')
+            ->inRandomOrder()
+            ->limit(10)
+            ->get();
+        if (count($dataProduk) > 0) {
             $meta = [
                 'code' => 200,
                 'message' => 'Success',
@@ -40,7 +41,7 @@ class ProductNonAuthController extends Controller
             $res['meta'] = $meta;
             $res['data'] = $data;
             return response($res);
-		}else{
+        } else {
             $meta = [
                 'code' => 204,
                 'message' => 'Data Not Found',
@@ -50,23 +51,24 @@ class ProductNonAuthController extends Controller
             $res['meta'] = $meta;
             $res['data'] = $data;
             return response($res);
-		}
-	}
+        }
+    }
 
-	public function browseProductByKategori(Request $request){
-		// echo $request->id_kategori;die();
-		// $id_kategori = $request->input('id_kategori');;
-		// dd($request->id_kategori);
-		$dataProduk = DB::table('itdp_company_users')
-							->join('csc_product_single', 'itdp_company_users.id', '=', 'csc_product_single.id_itdp_company_user')
-							->where('itdp_company_users.status', '=', 1)
-							->where('csc_product_single.status', 2)
-							->where('csc_product_single.id_csc_product', $request->id_kategori)
-							->select('csc_product_single.id', 'csc_product_single.prodname_en',
-							'csc_product_single.image_1', 'csc_product_single.id_csc_product', 'itdp_company_users.type')
-							->orderBy('csc_product_single.prodname_en','asc')
-							->get();
-		if(count($dataProduk) > 0){
+    public function browseProductByKategori(Request $request)
+    {
+        // echo $request->id_kategori;die();
+        // $id_kategori = $request->input('id_kategori');;
+        // dd($request->id_kategori);
+        $dataProduk = DB::table('itdp_company_users')
+            ->join('csc_product_single', 'itdp_company_users.id', '=', 'csc_product_single.id_itdp_company_user')
+            ->where('itdp_company_users.status', '=', 1)
+            ->where('csc_product_single.status', 2)
+            ->where('csc_product_single.id_csc_product', $request->id_kategori)
+            ->select('csc_product_single.id', 'csc_product_single.prodname_en',
+                'csc_product_single.image_1', 'csc_product_single.id_csc_product', 'itdp_company_users.type')
+            ->orderBy('csc_product_single.prodname_en', 'asc')
+            ->get();
+        if (count($dataProduk) > 0) {
             $meta = [
                 'code' => 200,
                 'message' => 'Success',
@@ -76,7 +78,7 @@ class ProductNonAuthController extends Controller
             $res['meta'] = $meta;
             $res['data'] = $data;
             return response($res);
-		}else{
+        } else {
             $meta = [
                 'code' => 204,
                 'message' => 'Data Not Found',
@@ -86,16 +88,17 @@ class ProductNonAuthController extends Controller
             $res['meta'] = $meta;
             $res['data'] = $data;
             return response($res);
-		}
-	}
+        }
+    }
 
-	public function findKategori(){
-		$result = DB::table('csc_product')
-						->where('level_1', 0)
-						->where('level_2', 0)
-						->orderBy('nama_kategori_en', 'ASC')
-						->get();
-		if(count($result) > 0){
+    public function findKategori()
+    {
+        $result = DB::table('csc_product')
+            ->where('level_1', 0)
+            ->where('level_2', 0)
+            ->orderBy('nama_kategori_en', 'ASC')
+            ->get();
+        if (count($result) > 0) {
             $meta = [
                 'code' => 200,
                 'message' => 'Success',
@@ -105,7 +108,7 @@ class ProductNonAuthController extends Controller
             $res['meta'] = $meta;
             $res['data'] = $data;
             return response($res);
-		}else{
+        } else {
             $meta = [
                 'code' => 204,
                 'message' => 'Data Not Found',
@@ -115,17 +118,17 @@ class ProductNonAuthController extends Controller
             $res['meta'] = $meta;
             $res['data'] = $data;
             return response($res);
-		}
-	}
+        }
+    }
 
-	public function detailProduk(Request $request)
+    public function detailProduk(Request $request)
     {
         //Product
         $data = DB::table('csc_product_single')
             ->where('id', '=', $request->id)
-			->first();
-			// dd($data);
-		if(count($data) > 0){
+            ->first();
+        // dd($data);
+        if (count($data) > 0) {
             $meta = [
                 'code' => 200,
                 'message' => 'Success',
@@ -135,7 +138,7 @@ class ProductNonAuthController extends Controller
             $res['meta'] = $meta;
             $res['data'] = $data;
             return response($res);
-		}else{
+        } else {
             $meta = [
                 'code' => 204,
                 'message' => 'Data Not Found',
@@ -145,35 +148,104 @@ class ProductNonAuthController extends Controller
             $res['meta'] = $meta;
             $res['data'] = $data;
             return response($res);
-		}
-	}
+        }
+    }
 
-	 public function getImageProduk($id, $image){
-		$path = public_path().'/uploads/Eksportir_Product/Image/'.$id.'/'.$image;
-		if(is_file($path)){
-			return response()->download($path);        
-		}else{
-			return response()->download(public_path().'/image/noimage.jpg');
-		}
-	}
-	
-	public function responseView($pathToFile,$filename)
+    public function getImageProduk($id, $image)
+    {
+        $path = public_path() . '/uploads/Eksportir_Product/Image/' . $id . '/' . $image;
+        if (is_file($path)) {
+            return response()->download($path);
+        } else {
+            return response()->download(public_path() . '/image/noimage.jpg');
+        }
+    }
+
+    public function responseView($pathToFile, $filename)
     {
 
-         $headers =['Access-Control-Allow-Origin'      => '*',
-                    'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS, PUT, DELETE',
-                    'Access-Control-Allow-Credentials' => 'true',
-                    'Access-Control-Max-Age'           => '86400',
-                    'Access-Control-Allow-Headers'     => 'Content-Type, Accept, Authorization, X-Requested-With, Application, Origin, Authorization, APIKey, Timestamp, AccessToken',
-                    'Content-Disposition' => 'filename='.$filename, 
-                    'Pragma' => 'public',
-                    'Content-Transfer-Encoding' => 'binary',
-                    'Content-Type' =>   $this->getContentType($pathToFile),
-                    'Content-Length' => filesize($pathToFile)];
+        $headers = ['Access-Control-Allow-Origin' => '*',
+            'Access-Control-Allow-Methods' => 'POST, GET, OPTIONS, PUT, DELETE',
+            'Access-Control-Allow-Credentials' => 'true',
+            'Access-Control-Max-Age' => '86400',
+            'Access-Control-Allow-Headers' => 'Content-Type, Accept, Authorization, X-Requested-With, Application, Origin, Authorization, APIKey, Timestamp, AccessToken',
+            'Content-Disposition' => 'filename=' . $filename,
+            'Pragma' => 'public',
+            'Content-Transfer-Encoding' => 'binary',
+            'Content-Type' => $this->getContentType($pathToFile),
+            'Content-Length' => filesize($pathToFile)];
 
-        $response = new BinaryFileResponse($pathToFile, 200 , $headers);
+        $response = new BinaryFileResponse($pathToFile, 200, $headers);
         return $response;
 
+    }
+
+    public function getRandomProduct()
+    {
+        $data = DB::table('csc_product_single')
+            ->join('itdp_company_users', 'itdp_company_users.id', '=', 'csc_product_single.id_itdp_company_user')
+            ->select('csc_product_single.*', 'itdp_company_users.id as id_company', 'itdp_company_users.status as status_company')
+            ->where('itdp_company_users.status', 1)
+            ->where('csc_product_single.status', 2)
+            ->inRandomOrder()
+            ->limit(6)
+            ->get();
+        // dd($data);
+        if (count($data) > 0) {
+            $meta = [
+                'code' => 200,
+                'message' => 'Success',
+                'status' => 'OK'
+            ];
+//            $data = $dataProduk;
+            $res['meta'] = $meta;
+            $res['data'] = $data;
+            return response($res);
+        } else {
+            $meta = [
+                'code' => 204,
+                'message' => 'Data Not Found',
+                'status' => 'Failed'
+            ];
+//            $data = data;
+            $res['meta'] = $meta;
+            $res['data'] = $data;
+            return response($res);
+        }
+    }
+
+    public function getprodukBaru()
+    {
+        $data = DB::table('csc_product_single')
+            ->join('itdp_company_users', 'itdp_company_users.id', '=', 'csc_product_single.id_itdp_company_user')
+            ->select('csc_product_single.*', 'itdp_company_users.id as id_company', 'itdp_company_users.status as status_company')
+            ->where('itdp_company_users.status', 1)
+            ->where('csc_product_single.status', 2)
+            ->orderBy('csc_product_single.created_at', 'DESC')
+            ->limit(6)
+            ->get();
+        // dd($data);
+        if (count($data) > 0) {
+            $meta = [
+                'code' => 200,
+                'message' => 'Success',
+                'status' => 'OK'
+            ];
+//            $data = $dataProduk;
+            $res['meta'] = $meta;
+            $res['data'] = $data;
+            return response($res);
+        } else {
+            $meta = [
+                'code' => 204,
+                'message' => 'Data Not Found',
+                'status' => 'Failed'
+            ];
+//            $data = data;
+            $res['meta'] = $meta;
+            $res['data'] = $data;
+            return response($res);
+        }
     }
 
 }
