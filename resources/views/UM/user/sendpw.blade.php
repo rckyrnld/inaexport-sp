ad87ab8eeadc8619e580e15a0edc8086c866ab62
fahri <fahrisafari95@gmail.com>
2019-12-02 10:27:22 +0700

    email ke perwakilan saat pembuatan akun , error didetail transaksi admin & perwakilan , ubah label kecil kecil

---
+++ resources/views/UM/user/sendpw.blade.php
@@ -0,0 +1,8 @@
+<p>Dear Representative, </P>
+<p>Admin created and set you as Representative !</p>
+<ol>
+    <ul>Mail : {{$email1}}</ul>
+    <ul>Username : {{$username}}</ul>
+    <ul>Password : {{$password}}</ul>
+</ol>
+click <a href="{{url('admin')}}">here</a> for try to login !.
--- resources/views/trx/trx.blade.php
+++ resources/views/trx/trx.blade.php
@@ -238,11 +238,11 @@ $cr = explode(',',$p3->id_csc_prod);
 			<b>Quantity</b>
 		</div>
 		<div class="form-group col-sm-2">
-			<input type="number" name="eo" class="form-control" value="<?php  echo $p3->eo; ?>" readonly>
+			<input type="number" name="eo" class="form-control" value="<?php  echo $p3->eo; ?>">
 		</div>
 		<div class="form-group col-sm-1">
 			<input type="hidden" name="id_br" class="form-control" value="<?php echo $p2->id_terkait; ?>">
-			 <select class="form-control" name="neo" id="neo" readonly>
+			 <select class="form-control select2" name="neo" id="neo">
                                                 <option value="">- Choose -</option>
 
                                                 <option <?php if($p3->neo == "Each"){ echo "selected"; }?> value="Each">Each</option>
@@ -288,10 +288,10 @@ $cr = explode(',',$p3->id_csc_prod);
 			<b>Price</b>
 		</div>
 		<div class="form-group col-sm-2">
-			<input type="number" name="tp" class="form-control" value="<?php  echo $p3->tp; ?>" readonly>
+			<input type="text" name="tp" class="amount form-control" value="<?php  echo number_format($p3->tp,0,',','.'); ?>">
 		</div>
 		<div class="form-group col-sm-1">
-			<select style="color:black;" readonly class="form-control" name="ntp" id="ntp"><option <?php if($p3->ntp == "IDR"){ echo "selected"; }?> value="IDR">IDR</option><option <?php if($p3->ntp == "THB"){ echo "selected"; }?> value="THB">THB</option><option <?php if($p3->ntp == "USD"){ echo "selected"; }?> value="USD">USD</option></select>
+			<select style="color:black;" class="form-control" name="ntp" id="ntp"><option <?php if($p3->ntp == "IDR"){ echo "selected"; }?> value="IDR">IDR</option><option <?php if($p3->ntp == "THB"){ echo "selected"; }?> value="THB">THB</option><option <?php if($p3->ntp == "USD"){ echo "selected"; }?> value="USD">USD</option></select>
 		</div>
 	</div>
 	
@@ -325,6 +325,107 @@ $cr = explode(',',$p3->id_csc_prod);
 	
 	<?php } ?>
 	
+	<?php }else { ?>
+	<!-- Inquiry -->
+	<div class="form-row">
+		<div class="form-group col-sm-2">
+			<b>Created By</b>
+		</div>
+		<div class="form-group col-sm-4">
+			<?php  if($p2->by_role == 1){ echo "Admin"; }else if($p2->by_role == 4){ echo "Representative"; }else{ echo "Importer"; }  ?>
+			<?php if($p2->by_role == 3){ 
+			$carih = DB::select("select a.*,b.* from itdp_company_users a, itdp_profil_imp b where a.id_profil=b.id and a.id='".$p2->id_pembuat."'");
+			foreach($carih as $ch){
+				echo " - ".$ch->badanusaha." ".$ch->company." (".$ch->username.")";
+			?>
+			
+			<?php } } ?>
+		</div>
+	</div>
+	<?php 
+	$idt = $p2->id_terkait;
+	$caridt = DB::select("select * from csc_inquiry_br where id='".$idt."'");
+	foreach($caridt as $cdt){
+		$cd1 = $cdt->id;
+		$cd2 = $cdt->id_csc_prod_cat;
+		$cd3 = $cdt->id_csc_prod_cat_level1;
+		$cd4 = $cdt->id_csc_prod_cat_level2;
+	}
+	//echo $cd1."aaaa";
+	?>
+	<div class="form-row">
+		<div class="form-group col-sm-2">
+			<b>Category</b>
+		</div>
+		<div class="form-group col-sm-4">
+			
+		</div>
+	</div>
+	<div class="form-row">
+		<div class="form-group col-sm-2">
+			<b>Quantity</b>
+		</div>
+		<div class="form-group col-sm-2">
+			<input type="number" name="eo" class="form-control" value="<?php  if(empty($p2->eo)){ echo "1"; }else{ echo $p2->eo; } ?>">
+		</div>
+		<div class="form-group col-sm-2">
+			<input type="hidden" name="id_in" class="form-control" value="<?php echo $p2->id_terkait; ?>">
+			 <select class="form-control" name="neo" id="neo">
+                                                <option value="">- Choose -</option>
+
+                                                <option <?php if($p2->neo == "Each"){ echo "selected"; }?> value="Each">Each</option>
+                                                <option <?php if($p2->neo == "Foot"){ echo "selected"; }?> value="Foot">Foot</option>
+                                                <option <?php if($p2->neo == "Gallons"){ echo "selected"; }?> value="Gallons">Gallons</option>
+                                                <option <?php if($p2->neo == "Kilograms"){ echo "selected"; }?>value="Kilograms">Kilograms</option>
+                                                <option <?php if($p2->neo == "Liters"){ echo "selected"; }?> value="Liters">Liters</option>
+                                                <option <?php if($p2->neo == "Packs"){ echo "selected"; }?>value="Packs">Packs</option>
+                                                <option <?php if($p2->neo == "Pairs"){ echo "selected"; }?> value="Pairs">Pairs</option>
+                                                <option <?php if($p2->neo == "Pieces"){ echo "selected"; }?> value="Pieces">Pieces</option>
+                                                <option <?php if($p2->neo == "Reams"){ echo "selected"; }?> value="Reams">Reams</option>
+                                                <option <?php if($p2->neo == "Rods"){ echo "selected"; }?> value="Rods">Rods</option>
+                                                <option <?php if($p2->neo == "Rolls"){ echo "selected"; }?> value="Rolls">Rolls</option>
+                                                <option <?php if($p2->neo == "Sets"){ echo "selected"; }?> value="Sets">Sets</option>
+                                                <option <?php if($p2->neo == "Sheets"){ echo "selected"; }?>value="Sheets">Sheets</option>
+                                                <option <?php if($p2->neo == "Square Meters"){ echo "selected"; }?> value="Square Meters">Square Meters</option>
+                                                <option <?php if($p2->neo == "Tons"){ echo "selected"; }?> value="Tons">Tons</option>
+                                                <option <?php if($p2->neo == "Unit"){ echo "selected"; }?> value="Unit">Unit</option>
+                                                <option <?php if($p2->neo == "令"){ echo "selected"; }?> value="令">令</option>
+                                                <option <?php if($p2->neo == "件"){ echo "selected"; }?> value="件">件</option>
+                                                <option <?php if($p2->neo == "加仑"){ echo "selected"; }?> value="加仑">加仑</option>
+                                                <option <?php if($p2->neo == "包"){ echo "selected"; }?> value="包">包</option>
+                                                <option <?php if($p2->neo == "千克"){ echo "selected"; }?> value="千克">千克</option>
+                                                <option <?php if($p2->neo == "升"){ echo "selected"; }?> value="升">升</option>
+                                                <option <?php if($p2->neo == "单位"){ echo "selected"; }?> value="单位">单位</option>
+                                                <option <?php if($p2->neo == "卷"){ echo "selected"; }?> value="卷">卷</option>
+                                                <option <?php if($p2->neo == "吨"){ echo "selected"; }?> value="吨">吨</option>
+                                                <option <?php if($p2->neo == "套"){ echo "selected"; }?> value="套">套</option>
+                                                <option <?php if($p2->neo == "对"){ echo "selected"; }?> value="对">对</option>
+                                                <option <?php if($p2->neo == "平方米"){ echo "selected"; }?> value="平方米">平方米</option>
+                                                <option <?php if($p2->neo == "张"){ echo "selected"; }?> value="张">张</option>
+                                                <option <?php if($p2->neo == "根"){ echo "selected"; }?> value="根">根</option>
+                                                <option <?php if($p2->neo == "每个"){ echo "selected"; }?> value="每个">每个</option>
+                                                <option <?php if($p2->neo == "英尺"){ echo "selected"; }?> value="英尺">英尺</option>
+                                                <option <?php if($p2->neo == "集装箱"){ echo "selected"; }?> value="集装箱">集装箱</option>
+
+                                            </select>
+		</div>
+	</div>
+	
+	<div class="form-row">
+		<div class="form-group col-sm-2">
+			<b>Price</b>
+		</div>
+		<div class="form-group col-sm-2">
+		
+	<input type="text" name="tp" class="amount form-control" value="<?php  if(empty($p2->tp) || $p2->tp == null ){ echo "0"; }else{ echo number_format($p2->tp,0,',','.'); } ?>">
+		</div>
+		<div class="form-group col-sm-2">
+			<select style="color:black;" class="form-control" name="ntp" id="ntp"><option value="IDR">IDR</option><option value="THB">THB</option><option selected value="USD">USD</option></select>
+		</div>
+	</div>
+	
+	
+	<?php } ?>
 	
 	
 	<div class="form-row">
@@ -332,7 +433,7 @@ $cr = explode(',',$p3->id_csc_prod);
 			<b>Tracking of Type</b>
 		</div>
 		<div class="form-group col-sm-3">
-			<select <?php if($p2->status_transaksi == 1){ echo "readonly"; }?> class="form-control" name="type_tracking" readonly>
+			<select <?php if($p2->status_transaksi == 1){ echo "readonly"; }?> class="form-control" name="type_tracking">
 				<option value="">- Select Tracking Type -</option>
 				<option <?php if($p2->type_tracking == "DHL Express"){ echo "selected"; }?> value="DHL Express">DHL Express</option>
 				<option <?php if($p2->type_tracking == "DHL Active Tracing"){ echo "selected"; }?> value="DHL Active Tracing">DHL Active Tracing</option>
@@ -352,7 +453,7 @@ $cr = explode(',',$p3->id_csc_prod);
 			<b>No Tracking</b>
 		</div>
 		<div class="form-group col-sm-3">
-			<input class="form-control" readonly type="text" id="no_track" name="no_track" value="<?php echo $p2->no_tracking; ?>" <?php if($p2->status_transaksi == 1){ echo "readonly"; }?>>
+			<input class="form-control" type="text" id="no_track" name="no_track" value="<?php echo $p2->no_tracking; ?>" <?php if($p2->status_transaksi == 1){ echo "readonly"; }?>>
 			<input class="form-control" type="hidden" id="tipekirim" name="tipekirim" value="0">
 			<input class="form-control" type="hidden" id="id_transaksi" name="id_transaksi" value="<?php echo $p2->id_transaksi;?>">
 		</div>
@@ -373,7 +474,51 @@ $cr = explode(',',$p3->id_csc_prod);
 
 <?php } ?>
 </form>
+<script>
+    function formatAmountNoDecimals( number ) {
+    var rgx = /(\d+)(\d{3})/;
+    while( rgx.test( number ) ) {
+        number = number.replace( rgx, '$1' + '.' + '$2' );
+    }
+    return number;
+}
 
+function formatAmount( number ) {
+
+    // remove all the characters except the numeric values
+    number = number.replace( /[^0-9]/g, '' );
+
+    // set the default value
+    if( number.length == 0 ) number = "0.00";
+    else if( number.length == 1 ) number = "0.0" + number;
+    else if( number.length == 2 ) number = "0." + number;
+    else number = number.substring( 0, number.length - 2 ) + '.' + number.substring( number.length - 2, number.length );
+	
+    // set the precision
+    number = new Number( number );
+    number = number.toFixed( 2 );    // only works with the "."
+
+    // change the splitter to ","
+    number = number.replace( /\./g, '' );
+
+    // format the amount
+    x = number.split( ',' );
+    x1 = x[0];
+    x2 = x.length > 1 ? ',' + x[1] : '';
+
+    return formatAmountNoDecimals( x1 ) + x2;
+}
+
+
+$(function() {
+
+    $( '.amount' ).keyup( function() {
+        $( this ).val( formatAmount( $( this ).val() ) );
+    });
+
+});
+
+</script>
 <script>
 function getyou(x){
 	$('#tipekirim').val(x);
