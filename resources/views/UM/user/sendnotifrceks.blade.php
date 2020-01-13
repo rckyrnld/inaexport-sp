a04f259c3f14fcbb44f611b6d5d70fa07808ccb1
mindy <mindyatikah@gmail.com>
2020-01-13 17:10:04 +0700

    mindy (notif admin 1)

---
+++ resources/views/UM/user/sendnotifrceks.blade.php
@@ -0,0 +1,6 @@
+<p>Yth. {{$username}}</p>
+<br>
+<p> Terdapat dokumen baru yang diterbitkan di Research Corner dengan judul {{$judul}}</p>
+Klik <a href="{{url('research-corner/list')}}">disini</a> untuk mengaksesnya
+{{--click <a href="{{url('')}}">Here</a>.--}}
+
