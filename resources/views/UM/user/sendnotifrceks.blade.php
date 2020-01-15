841d0b03baa0ac717a11b667553bf74aceb81cd8
mindy <mindyatikah@gmail.com>
2020-01-15 14:08:20 +0700

    mindy

--- resources/views/UM/user/sendnotifrceks.blade.php
+++ resources/views/UM/user/sendnotifrceks.blade.php
@@ -1,10 +1,9 @@
-<img height="20%" width="100%" src="{{url('assets')}}/assets/images/headeremail.jpg" alt="." >
-<img height="50%" width="100%" src="{{url('assets')}}/assets/images/iconpengumuman.jpg" alt="." >
+<img height="100%" width="580px" src="{{url('assets')}}/assets/images/headeremail2.jpg" alt="." >
 <p style="color: #8bbbe8; font-size: 20px;">Information</p>
 <hr>
 <p>Yth. {{$username}}</p>
 <br>
 <p> Terdapat dokumen baru yang diterbitkan di Research Corner dengan judul {{$judul}}</p>
 Klik <a href="{{url('research-corner/list')}}">disini</a> untuk mengaksesnya
-<img height="30%" width="100%" src="{{url('assets')}}/assets/images/footeremail.jpg" alt="." >
+<img height="100%" width="580px" src="{{url('assets')}}/assets/images/footeremail2.jpg" alt="." >
 
