7fca983ecfc5734615721f3c7881833a39428106
mindy <mindyatikah@gmail.com>
2020-01-15 14:29:13 +0700

    mindy

--- resources/views/UM/user/sendnotifrceks.blade.php
+++ resources/views/UM/user/sendnotifrceks.blade.php
@@ -1,9 +1,16 @@
-<img height="100%" width="580px" src="{{url('assets')}}/assets/images/headeremail2.jpg" alt="." >
-<p style="color: #8bbbe8; font-size: 20px;">Information</p>
-<hr>
-<p>Yth. {{$username}}</p>
-<br>
-<p> Terdapat dokumen baru yang diterbitkan di Research Corner dengan judul {{$judul}}</p>
-Klik <a href="{{url('research-corner/list')}}">disini</a> untuk mengaksesnya
-<img height="100%" width="580px" src="{{url('assets')}}/assets/images/footeremail2.jpg" alt="." >
+<div align="center" style="width: 100%">
+    <div align="center" style="width: 580px;">
+        <img height="100%" width="580px" src="{{url('assets')}}/assets/images/headeremail2.jpg" alt="." >
+        <p style="color: #8bbbe8; font-size: 20px;text-align: left;">Information</p>
+        <hr>
+        <p style="text-align: left">Yth. {{$username}}</p>
+        <br>
+        <p style="text-align: left"> Terdapat dokumen baru yang diterbitkan di Research Corner dengan judul {{$judul}}</p>
+        <p style="text-align: left">Klik <a href="{{url('research-corner/list')}}">disini</a> untuk mengaksesnya</p>
+        <img height="100%" width="580px" src="{{url('assets')}}/assets/images/footeremail2.jpg" alt="." >
+    </div>
+</div>
+
+
+
 
