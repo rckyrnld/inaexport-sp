6e99fa9ffe5a3cd03da7e8b9418408086e6113bc
mindy <mindyatikah@gmail.com>
2020-01-24 11:02:29 +0700

    mindy

--- resources/views/UM/user/emailexupload.blade.php
+++ resources/views/UM/user/emailexupload.blade.php
@@ -3,7 +3,7 @@
         <img height="100%" width="580px" src="{{url('assets')}}/assets/images/headeremail2.jpg" alt="." >
         <p style="text-align: left">Dear Admin,</p>
 {{--        <p style="text-align: left">There is new file uploaded by {{($bu== '-')? '':$bu." "}}{{$company}}. </p>--}}
-        <p style="text-align: left">Exporter {{($bu== '-')? '':$bu." "}}{{$company}} Edit Profile or Upload New File. </p>
+        <p style="text-align: left">Exporter {{($bu== '-')? '':$bu." "}}{{$company}} Update Company Data. </p>
 {{--        <p style="text-align: left;">Yth. {{$company}},</p>--}}
 
 {{--        <p style="text-align: left;">Terima kasih menggunakan layanan Ticketing inaexport. Nomor tiket anda adalah {{$ticketing}}</p>--}}
@@ -20,7 +20,7 @@
 {{--          {{$main_messages}}--}}
 {{--        </p>--}}
 {{--        <hr>--}}
-        <p style="text-align: left">Click <a href="{{url('profil/2', $id)}}">here to see the new profile</a></p>
+        <p style="text-align: left">Click <a href="{{url('profil/2', $id)}}">here</a> to see the new profile</p>
 {{--        <p style="text-align: left;>click <a href="{{url('admin/ticketing/chatview', $id)}}">Here</a>.</p>--}}
         <img height="100%" width="580px" src="{{url('assets')}}/assets/images/footeremail2.jpg" alt="." >
     </div>
