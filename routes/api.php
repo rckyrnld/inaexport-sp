d753a92337b9073b96dd8a33dad3570d8da148f6
fahri <fahrisafari95@gmail.com>
2020-02-03 16:37:55 +0700

    commit

--- routes/api.php
+++ routes/api.php
@@ -46,6 +46,12 @@ Route::group(['middleware' => ['api', 'manage_token:api_admin,1|4']], function (
 	Route::post('list_br_join', 'Api\Admin\ManagementController@list_br_join');
 	Route::post('list_br_chat', 'Api\Admin\ManagementController@list_br_chat');
 	Route::post('br_admin_save', 'Api\Admin\ManagementController@br_admin_save');
+	
+	//Inquiry 
+	Route::post('list_inquiry_admin', 'Api\Admin\InquiryController@list_inquiry_admin');
+	Route::post('insert_inquiry_admin', 'Api\Admin\InquiryController@store');
+	Route::post('list_inquiry_broadcast', 'Api\Admin\InquiryController@list_inquiry_broadcast');
+	Route::post('list_inquiry_hc', 'Api\Admin\InquiryController@list_inquiry_hc');
 
     // LIST COMPANY
     Route::post('listCompany', 'Api\Admin\ManagementController@listCompany');
