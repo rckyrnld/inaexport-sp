96277cf1fe93cb98c4d6fa8be17b355e171f3ed6
fahri <fahrisafari95@gmail.com>
2020-02-05 10:58:57 +0700

    commit

--- routes/api.php
+++ routes/api.php
@@ -63,6 +63,8 @@ Route::group(['middleware' => ['api', 'manage_token:api_admin,1|4']], function (
 
     // DETAIL EKS
     Route::post('detailCompany', 'Api\Admin\ManagementController@detailCompany');
+    Route::post('searchcompany', 'Api\Admin\EksreportController@searchcompany');
+    Route::post('searchproduct', 'Api\Admin\EksreportController@searchproduct');
 });
 
 Route::group(['middleware' => ['api', 'manage_token:api_user,2|3']], function () {
