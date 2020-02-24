514358c17a9df78bc61aa86e4011c956dd6f6055
fahri <fahrisafari95@gmail.com>
2020-02-24 09:41:30 +0700

    commit

--- app/Http/Controllers/Api/User/InquiryController.php
+++ app/Http/Controllers/Api/User/InquiryController.php
@@ -749,7 +749,12 @@ class InquiryController extends Controller
 			if($user[$i]->type == "admin"){
 				$jsonResult[$i]["id_type"] = 1;
 				$carid = DB::table('csc_inquiry_category')->where('id_inquiry', '=', $user[$i]->idb)->get();
-				foreach($carid as $c1){ $id_cad_prod = $c1->id_cad_prod; }
+				if(count($carid) == 0){
+				$jsonResult[$i]["id_csc_prod_cat"] = 0;
+				$jsonResult[$i]["id_csc_prod_cat_level1"] = 0;
+				$jsonResult[$i]["id_csc_prod_cat_level2"] = 0;
+				}else{
+				foreach($carid as $c1){ $id_cad_prod = $c1->id_cat_prod; }
 				$ambilcat = DB::table('csc_product')->where('id', '=', $id_cad_prod)->get();
 				foreach($ambilcat as $c2){
 					$ip1 = $c2->level_2;
@@ -759,11 +764,17 @@ class InquiryController extends Controller
 				$jsonResult[$i]["id_csc_prod_cat"] = $ip1;
 				$jsonResult[$i]["id_csc_prod_cat_level1"] = $ip2;
 				$jsonResult[$i]["id_csc_prod_cat_level2"] = $ip3;
+				}
 				
 			}else if($user[$i]->type == "perwakilan"){
 				$jsonResult[$i]["id_type"] = 4;
 				$carid = DB::table('csc_inquiry_category')->where('id_inquiry', '=', $user[$i]->idb)->get();
-				foreach($carid as $c1){ $id_cad_prod = $c1->id_cad_prod; }
+				if(count($carid) == 0){
+				$jsonResult[$i]["id_csc_prod_cat"] = 0;
+				$jsonResult[$i]["id_csc_prod_cat_level1"] = 0;
+				$jsonResult[$i]["id_csc_prod_cat_level2"] = 0;
+				}else{
+				foreach($carid as $c1){ $id_cad_prod = $c1->id_cat_prod; }
 				$ambilcat = DB::table('csc_product')->where('id', '=', $id_cad_prod)->get();
 				foreach($ambilcat as $c2){
 					$ip1 = $c2->level_2;
@@ -773,6 +784,7 @@ class InquiryController extends Controller
 				$jsonResult[$i]["id_csc_prod_cat"] = $ip1;
 				$jsonResult[$i]["id_csc_prod_cat_level1"] = $ip2;
 				$jsonResult[$i]["id_csc_prod_cat_level2"] = $ip3;
+				}
 			}else{
 				$jsonResult[$i]["id_type"] = 3;
 				$jsonResult[$i]["id_csc_prod_cat"] = $user[$i]->id_csc_prod_cat;
