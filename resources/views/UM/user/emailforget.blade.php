1cfad394cd67ffb2bc852b6a1d1c1faf8ef7ab82
Ricky Rinaldi <rickyrinaldi.net@gmail.com>
2020-06-05 10:41:25 +0700

    Update text emailforget.blade.php

--- resources/views/UM/user/emailforget.blade.php
+++ resources/views/UM/user/emailforget.blade.php
@@ -1,7 +1,10 @@
-<p>Dear Member, </P>
-<p>You have accessed the Forget Password feature</p>
-<ol>
-    <ul>Name : {{$nama}}</ul>
-    <ul>Username : {{$username}}</ul>
-</ol>
-click <a href="{{url('gantipass1/'.$id2)}}">here</a> to chages your password !.
+<p>Dear {{$nama}}, </P>
+<p>
+You're receiving this email because you requested a password reset for your InaExport Account. If you did not request this change, you can safely ignore this email.</p>
+<p>
+To choose a new password and complete your request, please follow the link below:<br>
+<a href="{{url('gantipass1/'.$id2)}}">Reset Password</a>
+</p>
+<p>
+You can change your password again at any time from within your My Account at http://inaexport.id. 
+</p>
