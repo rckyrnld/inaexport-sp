46019f188a35fc705a3131d55e9f9b32499252e2
mindy <mindyatikah@gmail.com>
2020-08-25 14:06:18 +0700

    mindy(membuat tablenya tidak lebih dari lebar layar)

--- resources/views/eksportir/eksproduct/index_admin_un.blade.php
+++ resources/views/eksportir/eksproduct/index_admin_un.blade.php
@@ -13,46 +13,46 @@
                        style="background-color: #1089ff; color: white;"><i
                                 class="fa fa-plus-circle"></i>
                         Tambah</a> -->
-                    <div class="col-md-14">
-                        <div class="table-responsive">
-                            <table id="tablebrands" class="table  table-bordered table-striped">
+                    <div class="col-md-12" style="max-width: 100%;">
+                        <!-- <div class="table-responsive"> -->
+                            <table width="320px" id="tablebrands" class="table  table-bordered table-striped">
                                 <thead class="text-white" style="background-color: #1089ff;">
                                 <tr>
-                                    <th>No</th>
-                                    <th>
+                                    <th style="width:20px!important;padding-left:2px;padding-right:5px;">No</th>
+                                    <th  style="width:30px!important;padding-left:2px;padding-right:5px;">
                                         Code
                                     </th>
-                                    <th>
+                                    <th  style="width:30px!important;padding-left:2px;padding-right:5px;">
                                         Product Name
                                     </th>
-                                    <th>
+                                    <th style="width:20px!important;padding-left:2px;padding-right:5px;">
                                         Company Name
                                     </th>
-                                    <th>
+                                    <th style="width:20px!important;padding-left:2px;padding-right:5px;">
                                         Color
                                     </th>
-                                    <th>
+                                    <th style="width:30px!important;padding-left:2px;padding-right:5px;">
                                         Size
                                     </th>
-                                    <th>
+                                    <th style="width:30px!important;padding-left:2px;padding-right:5px;">
                                         Raw Material
                                     </th>
-                                    <th>
+                                    <th style="width:30px!important;padding-left:2px;padding-right:5px;">
                                         Capacity
                                     </th>
-                                    <th>
+                                    <th style="width:20px!important;padding-left:2px;padding-right:5px;">
                                         Price (USD)
                                     </th>
-                                    <th>
+                                    <th style="width:30px!important;padding-left:2px;padding-right:5px;">
                                         Description Product
                                     </th>
-                                    <th>
+                                    <th style="width:20px!important;padding-left:2px;padding-right:5px;">
                                         Status
                                     </th>
-                                    <th>
+                                    <th style="width:20px!important;padding-left:2px;padding-right:5px;">
                                         Information
                                     </th>
-                                    <th>
+                                    <th style="width:20px!important;padding-left:2px;padding-right:5px;">
                                         Action
                                     </th>
                                 </tr>
@@ -61,7 +61,7 @@
                             </table>
                             <br>
                             
-                        </div>
+                        <!-- </div> -->
                     </div>
                 </div>
             </div>
@@ -76,22 +76,60 @@
         $('#tablebrands').DataTable({
             processing: true,
             serverSide: true,
+            // scrollX : true, 
+            scrollX: 200,
+            scroller: {
+                loadingIndicator: true
+            },
             ajax: "{{ route('datatables.eksproduct_admin_un', $id_profil) }}",
             columns: [
-                {data: 'DT_RowIndex', name: 'DT_RowIndex'},
-                {data: 'code_en', name: 'code_en'},
-                {data: 'prodname_en', name: 'prodname_en'},
-                {data: 'company_name', name: 'company_name'},
-                {data: 'color_en', name: 'color_en'},
-                {data: 'size_en', name: 'size_en'},
-                {data: 'raw_material_en', name: 'raw_material_en'},
-                {data: 'capacity', name: 'capacity'},
-                {data: 'price_usd', name: 'price_usd'},
-                {data: 'product_description', name: 'product_description'},
-                {data: 'status', name: 'status'},
-                {data: 'information', name: 'information'},
-                {data: 'action', name: 'action', orderable: false, searchable: false}
-            ]
+                {data: 'DT_RowIndex', name: 'DT_RowIndex',sWidth: '10px'},
+                {data: 'code_en', name: 'code_en',sWidth: '20px'},
+                {data: 'prodname_en', name: 'prodname_en',sWidth: '20px'},
+                {data: 'company_name', name: 'company_name',sWidth: '20px'},
+                {data: 'color_en', name: 'color_en',sWidth: '20px'},
+                {data: 'size_en', name: 'size_en',sWidth: '20px'},
+                {data: 'raw_material_en', name: 'raw_material_en',sWidth: '20px'},
+                {data: 'capacity', name: 'capacity',sWidth: '20px'},
+                {data: 'price_usd', name: 'price_usd',sWidth: '20px'},
+                {data: 'product_description', name: 'product_description',sWidth: '20px'},
+                {data: 'status', name: 'status',sWidth: '20px'},
+                {data: 'information', name: 'information',sWidth: '20px'},
+                {data: 'action', name: 'action', orderable: false, searchable: false,sWidth: '10px'},
+                
+            ],
+            bAutoWidth: false, 
+            // Columns : [
+            //     { sWidth: '5%' },
+            //     { sWidth: '10%' },
+            //     { sWidth: '10%' },
+            //     { sWidth: '10%' },
+            //     { sWidth: '10%' },
+            //     { sWidth: '10%' },
+            //     { sWidth: '10%' },
+            //     { sWidth: '10%' },
+            //     { sWidth: '10%' },
+            //     { sWidth: '5%' },
+            //     { sWidth: '5%' },
+            //     { sWidth: '5%' },
+            //     { sWidth: '5%' },
+
+            // ]
+            
         });
     });
 </script>
+<style>
+    /* #tablebrands.dataTable td:nth-child(1) {
+        width: 20px;
+        max-width: 20px;
+        word-break: break-all;
+        white-space: pre-line;
+    }
+    #tablebrands.dataTable td:nth-child(1) {
+        width: 70px;
+        max-width: 20px;
+        word-break: break-all;
+        white-space: pre-line;
+    } */
+</style>
